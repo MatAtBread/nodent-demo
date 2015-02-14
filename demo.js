eee19762483b87f6fd96b572c53c22cdf359a3d4
Ubuntu <ubuntu@ip-172-31-45-31.eu-west-1.compute.internal>
2015-02-14 17:31:05 +0000

    Initial commit

---
+++ demo.js
@@ -0,0 +1,51 @@
+var nodent = require('nodent')() ;
+var http = require('http') ;
+var fs = require('fs') ;
+
+function handle(req,res) {
+  var url = req.url.split("?") ;
+  switch(url[0]) {
+  case '/':
+    res.body = "" ;
+    req.on('data',function(data){ res.body += data.toString() }) ;
+    req.on('end',function(){
+      res.statusCode = 200 ;
+      res.setHeader("Content-type","text/html") ;
+      fs.readFile('www/index.html',function(err,data){
+          var code = unescape(url[1] || "") + (req.body || "") ;
+          if (code) {
+            data = data.toString().replace("//ES7 code",code) ;
+	  }
+          res.end(data.toString()) ;
+      }) ;    
+    }) ;
+    break ;
+
+  case '/es7':
+  case '/promise':
+    res.body = "" ;
+    req.on('data',function(data){ res.body += data.toString() }) ;
+    req.on('end',function(){
+      try {
+        var result = {} ;
+        result.compiled = nodent.compile(res.body,"source.js",2,{es7:true,promises:req.url=='/promise'}).code ;
+        res.statusCode = 200 ;
+        res.setHeader("Content-type","application/json") ;
+        res.end(JSON.stringify(result)) ;
+      } catch (ex) {
+        res.statusCode = 500 ;
+        res.setHeader("Content-type","text/plain") ;
+        res.end(ex.message) ;
+      }
+    }) ;
+    break ;
+
+  default:
+    res.statusCode = 404 ;
+    res.setHeader("Content-type","text/plain") ;
+    res.end("Not found: "+req.url) ;
+    break;
+  }
+} ;
+
+http.createServer(handle).listen(80) ;
