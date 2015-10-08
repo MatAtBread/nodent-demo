99e14e075228ae3133baebae22017369f8f44e13
matatbread <bread@mailed.me.uk>
2015-10-08 15:32:33 +0000

    Bump nodent, add classes.js example

---
+++ www/examples/classes.js
@@ -0,0 +1,26 @@
+"use strict";
+
+class Y {
+	constructor() {
+		this.type = "Y" ;
+	}
+	async is(p) { return p+"Y of type "+this.type }
+	async get info() { return await this.is('I am a ') }
+}
+
+class Z extends Y {
+	constructor() {
+		super() ;
+		this.type = "Z" ;
+	}
+	async is(p) { return p+"Z"+await super.is(" based on a ") }
+}
+
+async function test() {
+    var a = new Y() ;
+    var b = new Z() ;
+    console.log(await a.info,await b.info) ;
+}
+
+test().then() ;
+
